4ba61d5fe09993e682dbfca06b3930ea84c537f9
tututututu123 <laitu6996@gmail.com>
2026-09-10 10:01:34 +0700

    ok

--- obj/Debug/net8.0/eSchool.AssemblyInfo.cs
+++ obj/Debug/net8.0/eSchool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eSchool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+021947cb67697ecb885b38a1c7a29fcaa95d546b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c17c025d7664a671cc1126446fa618bb60b031")]
 [assembly: System.Reflection.AssemblyProductAttribute("eSchool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eSchool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
